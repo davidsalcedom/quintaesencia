aa52e65e4705d62b1a686ed82da0d4bf159d4823
David Salcedo Miranda <davidsalcedomiranda@MacBook-Air-de-David.local>
2023-09-09 16:22:12 +0200

    Updates

---
+++ precache-manifest.1a79bb6bf63a887aea4e44dfae724634.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6416d8ea464d44efc89",
+    "url": "/quintaesencia/static/css/main.bf6011d5.chunk.css"
+  },
+  {
+    "revision": "f6416d8ea464d44efc89",
+    "url": "/quintaesencia/static/js/main.f6416d8e.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/quintaesencia/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "eec4f606c29e5c5ea02f",
+    "url": "/quintaesencia/static/js/runtime~main.eec4f606.js"
+  },
+  {
+    "revision": "49eacb37c1947da666babb93b421222c",
+    "url": "/quintaesencia/static/media/back.49eacb37.webp"
+  },
+  {
+    "revision": "4e1c15475bca83c86f12ed24ea4114f2",
+    "url": "/quintaesencia/static/media/1-el-mago.4e1c1547.webp"
+  },
+  {
+    "revision": "dbd39d7a5796db2678ad0556124bf420",
+    "url": "/quintaesencia/static/media/2-la-papisa.dbd39d7a.webp"
+  },
+  {
+    "revision": "1beb1f32fd28c05c41eb7fac4053872f",
+    "url": "/quintaesencia/static/media/3-la-emperatriz.1beb1f32.webp"
+  },
+  {
+    "revision": "22a45666624cc39af0b19b9465eb893a",
+    "url": "/quintaesencia/static/media/4-el-emperador.22a45666.webp"
+  },
+  {
+    "revision": "c651756ab8065ba2e5b6736d266f5594",
+    "url": "/quintaesencia/static/media/5-el-papa.c651756a.webp"
+  },
+  {
+    "revision": "0a0df135040f4acbb586cbce83fef40e",
+    "url": "/quintaesencia/static/media/6-el-enamorado.0a0df135.webp"
+  },
+  {
+    "revision": "284c2fc0315f69b407db3dad353b9f7e",
+    "url": "/quintaesencia/static/media/7-el-carro.284c2fc0.webp"
+  },
+  {
+    "revision": "ba7631e043457a7624b19e17443c5d65",
+    "url": "/quintaesencia/static/media/8-la-justicia.ba7631e0.webp"
+  },
+  {
+    "revision": "9b9879ee4f872ad81a489cada050e1ef",
+    "url": "/quintaesencia/static/media/9-el-ermitano.9b9879ee.webp"
+  },
+  {
+    "revision": "e54c9a53bac4613392d792f729a6d8c8",
+    "url": "/quintaesencia/static/media/10-la-rueda-de-la-fortuna.e54c9a53.webp"
+  },
+  {
+    "revision": "0cbfc2a6a4168c3611444a7aa077b4ba",
+    "url": "/quintaesencia/static/media/11-la-fuerza.0cbfc2a6.webp"
+  },
+  {
+    "revision": "404dd1132ae7b45763def93782963af4",
+    "url": "/quintaesencia/static/media/12-el-colgado.404dd113.webp"
+  },
+  {
+    "revision": "769728b4f6e72d10792d46ad17ccf035",
+    "url": "/quintaesencia/static/media/13-la-muerte.769728b4.webp"
+  },
+  {
+    "revision": "6d97b27a3023e551dd74d3bacc9b6d68",
+    "url": "/quintaesencia/static/media/14-la-templanza.6d97b27a.webp"
+  },
+  {
+    "revision": "21414077f75eca287ac4987167ff62eb",
+    "url": "/quintaesencia/static/media/15-el-diablo.21414077.webp"
+  },
+  {
+    "revision": "76f421a75c5bd822a1d9badb1108b874",
+    "url": "/quintaesencia/static/media/16-la-torre.76f421a7.webp"
+  },
+  {
+    "revision": "93925444b3cf38265de658824e1dfafa",
+    "url": "/quintaesencia/static/media/17-la-estrella.93925444.webp"
+  },
+  {
+    "revision": "03663ad0da5594f793cb934affd4b78b",
+    "url": "/quintaesencia/static/media/18-la-luna.03663ad0.webp"
+  },
+  {
+    "revision": "36daaea7b2efccadfc8623fa3a672d77",
+    "url": "/quintaesencia/static/media/19-el-sol.36daaea7.webp"
+  },
+  {
+    "revision": "71fbaabf55ed9708bda7a670f5101633",
+    "url": "/quintaesencia/static/media/20-el-juicio.71fbaabf.webp"
+  },
+  {
+    "revision": "c1455b2aac81834463d807ed886b50ed",
+    "url": "/quintaesencia/static/media/21-el-mundo.c1455b2a.webp"
+  },
+  {
+    "revision": "5d737b132c4d028662bceff49b566950",
+    "url": "/quintaesencia/static/media/22-el-loco.5d737b13.webp"
+  },
+  {
+    "revision": "29c46e5e79c318adef972c49d13c87ac",
+    "url": "/quintaesencia/static/media/23-as-de-bastos.29c46e5e.webp"
+  },
+  {
+    "revision": "ef787abfc4eddfa733b9c9edc3a9f05d",
+    "url": "/quintaesencia/static/media/24-dos-de-bastos.ef787abf.webp"
+  },
+  {
+    "revision": "befdd972ded677ed4ad893b6e57df131",
+    "url": "/quintaesencia/static/media/25-tres-de-bastos.befdd972.webp"
+  },
+  {
+    "revision": "4fefd649e65fbf5307e719658ca00418",
+    "url": "/quintaesencia/static/media/26-cuatro-de-bastos.4fefd649.webp"
+  },
+  {
+    "revision": "1f7c8db38c07eb4ab46a48335baf6320",
+    "url": "/quintaesencia/static/media/27-cinco-de-bastos.1f7c8db3.webp"
+  },
+  {
+    "revision": "456df2df0cc3d47aa18a7db6de3e4b23",
+    "url": "/quintaesencia/static/media/28-seis-de-bastos.456df2df.webp"
+  },
+  {
+    "revision": "5338e743fcb72701ac2bd680cfe03911",
+    "url": "/quintaesencia/static/media/29-siete-de-bastos.5338e743.webp"
+  },
+  {
+    "revision": "f84b115b2782d06dda87b103da17c31b",
+    "url": "/quintaesencia/static/media/30-ocho-de-bastos.f84b115b.webp"
+  },
+  {
+    "revision": "ef4d99d290bfc19e241e5c671216c8ab",
+    "url": "/quintaesencia/static/media/31-nueve-de-bastos.ef4d99d2.webp"
+  },
+  {
+    "revision": "80138640813be37bc3cb26261bc56785",
+    "url": "/quintaesencia/static/media/32-diez-de-bastos.80138640.webp"
+  },
+  {
+    "revision": "6503dc875a9cb222c307abcd8d639a1a",
+    "url": "/quintaesencia/static/media/33-sota-de-bastos.6503dc87.webp"
+  },
+  {
+    "revision": "5ee2f4b28c53d7d8eef17227d037b0a9",
+    "url": "/quintaesencia/static/media/34-caballero-de-bastos.5ee2f4b2.webp"
+  },
+  {
+    "revision": "a58f0817448052cbb55afa2da7471198",
+    "url": "/quintaesencia/static/media/35-reina-de-bastos.a58f0817.webp"
+  },
+  {
+    "revision": "90ddfe8cf1e2ac1c85905b971de1a1db",
+    "url": "/quintaesencia/static/media/36-rey-de-bastos.90ddfe8c.webp"
+  },
+  {
+    "revision": "1b4c4aaa3a73c6befae3d38a7af764bc",
+    "url": "/quintaesencia/static/media/37-as-de-copas.1b4c4aaa.webp"
+  },
+  {
+    "revision": "cb5994075b3edd22ae280f1149b17104",
+    "url": "/quintaesencia/static/media/38-dos-de-copas.cb599407.webp"
+  },
+  {
+    "revision": "b9aed11f16b74dd2cf84d421c7040187",
+    "url": "/quintaesencia/static/media/39-tres-de-copas.b9aed11f.webp"
+  },
+  {
+    "revision": "54fe8fa71823a6a173386312eac8551f",
+    "url": "/quintaesencia/static/media/40-cuatro-de-copas.54fe8fa7.webp"
+  },
+  {
+    "revision": "055b8bc57106b18f54516ee142419468",
+    "url": "/quintaesencia/static/media/41-cinco-de-copas.055b8bc5.webp"
+  },
+  {
+    "revision": "04535d204a62d81f46e707948fa82a08",
+    "url": "/quintaesencia/static/media/42-seis-de-copas.04535d20.webp"
+  },
+  {
+    "revision": "1f9a2c6d532cd72317c8a7cb04586280",
+    "url": "/quintaesencia/static/media/43-siete-de-copas.1f9a2c6d.webp"
+  },
+  {
+    "revision": "bff1db0334c805183d2c63b8f41e66ea",
+    "url": "/quintaesencia/static/media/44-ocho-de-copas.bff1db03.webp"
+  },
+  {
+    "revision": "5f0b6ed26ac168512cd15a036f8ea766",
+    "url": "/quintaesencia/static/media/45-nueve-de-copas.5f0b6ed2.webp"
+  },
+  {
+    "revision": "a23ebcfef7335ea097219ac7f997effc",
+    "url": "/quintaesencia/static/media/46-diez-de-copas.a23ebcfe.webp"
+  },
+  {
+    "revision": "254010bcec3228a40c423399777a6900",
+    "url": "/quintaesencia/static/media/47-sota-de-copas.254010bc.webp"
+  },
+  {
+    "revision": "452a768431f33d46007d73371f86af7d",
+    "url": "/quintaesencia/static/media/48-caballero-de-copas.452a7684.webp"
+  },
+  {
+    "revision": "a952506ed9eb6e9291a96316c924f73f",
+    "url": "/quintaesencia/static/media/49-reina-de-copas.a952506e.webp"
+  },
+  {
+    "revision": "6e8204e8413a188b575b959669a950be",
+    "url": "/quintaesencia/static/media/50-rey-de-copas.6e8204e8.webp"
+  },
+  {
+    "revision": "fbf07195c14d5ae06cb72b4f59c14030",
+    "url": "/quintaesencia/static/media/51-as-de-oros.fbf07195.webp"
+  },
+  {
+    "revision": "012603a3f1e7494a4183980c31048443",
+    "url": "/quintaesencia/static/media/52-dos-de-oros.012603a3.webp"
+  },
+  {
+    "revision": "1155c9d55f828cef727f92f751f730e8",
+    "url": "/quintaesencia/static/media/53-tres-de-oros.1155c9d5.webp"
+  },
+  {
+    "revision": "87442d453aa0440ad5cd1a0a5ed72480",
+    "url": "/quintaesencia/static/media/54-cuatro-de-oros.87442d45.webp"
+  },
+  {
+    "revision": "86413d5a9c9fd7cc80d4567d2d4ee699",
+    "url": "/quintaesencia/static/media/55-cinco-de-oros.86413d5a.webp"
+  },
+  {
+    "revision": "dcf6fb5c6138d95f88728fecf43589de",
+    "url": "/quintaesencia/static/media/56-seis-de-oros.dcf6fb5c.webp"
+  },
+  {
+    "revision": "c334628a718dd268c8ad79b76ba0ce7b",
+    "url": "/quintaesencia/static/media/57-siete-de-oros.c334628a.webp"
+  },
+  {
+    "revision": "f9f63846426cec233b8f326af56b60c8",
+    "url": "/quintaesencia/static/media/58-ocho-de-oros.f9f63846.webp"
+  },
+  {
+    "revision": "b12b28d3ee52da101856f188088f1d2d",
+    "url": "/quintaesencia/static/media/59-nueve-de-oros.b12b28d3.webp"
+  },
+  {
+    "revision": "00f1442f40181bb347fff34e8fff743c",
+    "url": "/quintaesencia/static/media/60-diez-de-oros.00f1442f.webp"
+  },
+  {
+    "revision": "94487a13fedafd0cc5578c700c1e959c",
+    "url": "/quintaesencia/static/media/61-sota-de-oros.94487a13.webp"
+  },
+  {
+    "revision": "6f8d17c17c500e0b7c3c75ff864ec5bb",
+    "url": "/quintaesencia/static/media/62-caballero-de-oros.6f8d17c1.webp"
+  },
+  {
+    "revision": "7f258892ab17bf61047d11a41c53a848",
+    "url": "/quintaesencia/static/media/63-reina-de-oros.7f258892.webp"
+  },
+  {
+    "revision": "2b313e280aa41d2a7612eba58b3ea6b3",
+    "url": "/quintaesencia/static/media/64-rey-de-oros.2b313e28.webp"
+  },
+  {
+    "revision": "312f1207baf29d793b277ce04da9162b",
+    "url": "/quintaesencia/static/media/65-as-de-espadas.312f1207.webp"
+  },
+  {
+    "revision": "08185d845fb64a8169068248abc458ab",
+    "url": "/quintaesencia/static/media/66-dos-de-espadas.08185d84.webp"
+  },
+  {
+    "revision": "7ef05bb6e1513e6e0af6ce2bb3bba124",
+    "url": "/quintaesencia/static/media/67-tres-de-espadas.7ef05bb6.webp"
+  },
+  {
+    "revision": "579da0433cf5f810da378e5e3bf9e7b2",
+    "url": "/quintaesencia/static/media/68-cuatro-de-espadas.579da043.webp"
+  },
+  {
+    "revision": "9d810111b475664b4d5a5a65919caa43",
+    "url": "/quintaesencia/static/media/69-cinco-de-espadas.9d810111.webp"
+  },
+  {
+    "revision": "2bb1beed40e9cc0583e96926feb96a58",
+    "url": "/quintaesencia/static/media/70-seis-de-espadas.2bb1beed.webp"
+  },
+  {
+    "revision": "d926486f3820e24f15bd635e2bf8563f",
+    "url": "/quintaesencia/static/media/71-siete-de-espadas.d926486f.webp"
+  },
+  {
+    "revision": "d2e20fd21d9b96941ac4d91f71d49138",
+    "url": "/quintaesencia/static/media/72-ocho-de-espadas.d2e20fd2.webp"
+  },
+  {
+    "revision": "39a04b06441182d40dc19e31f34f0bd8",
+    "url": "/quintaesencia/static/media/73-nueve-de-espadas.39a04b06.webp"
+  },
+  {
+    "revision": "ed4255d627a2fe421fead2dc66c44f27",
+    "url": "/quintaesencia/static/media/74-diez-de-espadas.ed4255d6.webp"
+  },
+  {
+    "revision": "2f3cd192f066bbd18629415fcb89d24f",
+    "url": "/quintaesencia/static/media/75-sota-de-espadas.2f3cd192.webp"
+  },
+  {
+    "revision": "901bd43948c20d9201c5414a17f30cab",
+    "url": "/quintaesencia/static/media/76-caballero-de-espadas.901bd439.webp"
+  },
+  {
+    "revision": "543c63247bb43945d3c371c310e9f7bb",
+    "url": "/quintaesencia/static/media/77-reina-de-espadas.543c6324.webp"
+  },
+  {
+    "revision": "02091d5cae7a879b4eaa4bd77b51bfe0",
+    "url": "/quintaesencia/static/media/78-rey-de-espadas.02091d5c.webp"
+  },
+  {
+    "revision": "6e62b5ee25d39a78bf2f2c97d14bd796",
+    "url": "/quintaesencia/index.html"
+  }
+];
